ccee711e259b133b0ea4ed2dd3949ffe0c4f6b8c
icecoolr1 <48815062+icecoolr1@users.noreply.github.com>
2019-04-03 21:14:48 +0800

    Add files via upload

---
+++ 19.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a=0;
+	int b=0;
+	int max;
+	printf("------变量值互换------\n");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("a=%d\n",a);
+	printf("b=%d\n",b);
+	printf("互换前: a=%d  b=%d",a,b);
+	printf("互换后: a=%d  b=%d",b,a);
+	}
+
