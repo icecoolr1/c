709e7720456aab06b659857f10caed95a8239f81
icecoolr1 <48815062+icecoolr1@users.noreply.github.com>
2019-05-02 09:09:09 +0800

    Add files via upload

---
+++ 数组相加.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[5] = {0};
+	int b[5]= {0};
+	int c[5] ;
+	for( int i=0; i<5; i++){
+		cin>>b[5];
+	}
+	for(int i=0; i<5; i++){
+		cin>>a[5];
+	}
+	for(int i=0; i<5; i++){
+		c[5]=a[5]+b[5];
+		cout<<c[5];
+	}
+	
+}
